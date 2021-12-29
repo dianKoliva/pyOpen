e829646b7c7bc252c3cf87236886e6478c0ebec8
dianKoliva <dianpretty01@gmail.com>
2021-12-29 13:48:31 +0200

    new changes

--- read.py
+++ read.py
@@ -1,5 +1,11 @@
 import cv2 as cv
 
-img=cv.imread('imgs/one.jfif')
-cv.imshow("First",img)
+# reading images
+# img=cv.imread('imgs/one.jfif')
+# cv.imshow("First",img)
+
+
+
+# reading videos 
+
 cv.waitKey(0)
