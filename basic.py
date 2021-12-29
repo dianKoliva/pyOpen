281cc0b18eb38507b91dd43292c6e7aec98ab22a
dianKoliva <dianpretty01@gmail.com>
2021-12-29 15:18:54 +0200

    new changes

--- basic.py
+++ basic.py
@@ -1,11 +1,23 @@
 import cv2 as cv
 
-img=cv.imread('imgs/two.jfif')
-cv.imshow("First",img)
+
 
 # CONVERTING TO GREYSCALE
+# img=cv.imread('imgs/two.jfif')
+# cv.imshow("First",img)
+# gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray',gray)
 
-gray=cv.cvtColor(img,cv.COLOR_BG2GRAY)
-cv.imshow('Gray',gray)
+
+#bluring an image
+img=cv.imread('imgs/one.jfif')
+cv.imshow("First",img)
+blur=cv.GaussianBlur(img,(7,7),cv.BORDER_DEFAULT)
+# cv.imshow('Blur',blur)
+
+#edge cascade
+canny=cv.Canny(blur,125,175)
+cv.imshow("Canny edges",canny)
+
 
 cv.waitKey(0)
