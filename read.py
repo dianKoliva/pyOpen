6ed74be0eb09d7332f5f9511ea4ed3c1e2b4b3ed
dianKoliva <dianpretty01@gmail.com>
2021-12-29 13:55:04 +0200

    new changes

--- read.py
+++ read.py
@@ -8,5 +8,14 @@ import cv2 as cv
 
 # reading videos 
 
-capture=cv.VideoCapture()
+capture=cv.VideoCapture('vids/one.mp4')
+while True:
+    isTrue, frame=capture.read()
+    cv.imshow('Video',frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
 cv.waitKey(0)
