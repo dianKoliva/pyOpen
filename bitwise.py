cf781c3fc3fd87bb45e84df727aeec35bc67bbb9
dianKoliva <dianpretty01@gmail.com>
2022-01-02 10:31:52 +0200

    new changes

--- bitwise.py
+++ bitwise.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 blank=np.zeros((400,400),dtype='uint8')
-rec=cv.rectangle(blank.copy)
-
+rec=cv.rectangle(blank.copy,(30,30),(370,370),255,-1)
+cic=cv.circle(blank.copy,(200,200),200,255,-1)
+cv.imshow('Circle',cic)
+cv.imshow('Rectangle',rec)
 cv.waitKey(0)
