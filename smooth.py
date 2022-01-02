71ab8b6ce7bfe40edfcf9754d52399ddaa6c2d6e
dianKoliva <dianpretty01@gmail.com>
2022-01-02 10:17:22 +0200

    smoothening

--- smooth.py
+++ smooth.py
@@ -2,7 +2,16 @@ import cv2 as cv
 
 img =cv.imread('imgs/large.jfif')
 
+# averaging
 avrg=cv.blur(img,(5,5))
-cv.imshow("avg",avrg)
+# cv.imshow("avg",avrg)
 
+#gausian blur
+gauss=cv.GaussianBlur(img,(5,5),0)
+# cv.imshow("avg",gauss)
+
+
+# median blur
+med=cv.medianBlur(img,3)
+cv.imshow("avg",med)
 cv.waitKey(0)
