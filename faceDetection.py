e81259d71317802fc7478f765b72e7ff2c74e2aa
dianKoliva <dianpretty01@gmail.com>
2022-01-02 14:12:28 +0200

    new changes

--- faceDetection.py
+++ faceDetection.py
@@ -1,7 +1,16 @@
 import cv2 as cv
-img=cv.imread('imgs/1.jfif')
+img=cv.imread('imgs/g.jpg')
 
 
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow("tda",gray)
+
+
+haar_casc=cv.CascadeClassifier("haar_face.xml")
+faces=haar_casc.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+print(f'number of faces found ={len(faces)}')
+for(x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+
+cv.imshow("Detected faces",img)
+
 cv.waitKey(0)
