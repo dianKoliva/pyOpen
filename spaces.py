fcfe9a8f001fb35e365d493ea8c12f9a1557b984
dianKoliva <dianpretty01@gmail.com>
2022-01-02 10:03:14 +0200

    new changes

--- spaces.py
+++ spaces.py
@@ -1,9 +1,13 @@
 import cv2 as cv 
 
-img =cv.imread('imgs/one.jfif')
-cv.imshow("Image",img)
+img =cv.imread('imgs/two.jfif')
+# cv.imshow("Image",img)
 
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Gray",gray)
+
+#bgr to hsv 
+hsv=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+cv.imshow("hv",hsv)
 
 cv.waitKey(0)
