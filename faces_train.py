922e9a2b28dc82f2c56a868093ce62ddba1fedea
dianKoliva <dianpretty01@gmail.com>
2022-01-25 10:08:56 +0200

    NEW CHANGES

--- faces_train.py
+++ faces_train.py
@@ -1,3 +1,13 @@
 import os 
 import cv2 as cv
 import numpy as np
+
+people=['Barack Obama',"Michelle Obama","Paul Kagame"]
+
+p=[]
+
+for i in os.listdir(r'C:\Users\Prett\Documents\learn\pyOpen\faces'):
+    p.append(i)
+
+print(p)
+
